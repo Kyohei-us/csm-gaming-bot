9314a4e6aaf1bda8391108a03dde8d8a211b0351
Kyohei-us <kyohei1212000@gmail.com>
2019-10-23 15:36:06 -0700

    install discord.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ async def on_ready():
     target_channel = bot.get_channel(636689612659490816)
     await target_channel.send("I'm ready.")
 
-bot.run(os.environ.get('BOT_TOKEN'))
+bot.run(os.environ.get('NjM2Njg5MTEzMjIyNzQyMDM3.XbDRfQ.k7Ysq2OZBbBnqs2yQTsg6QwTsRA'))
