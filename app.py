96ec3fdc74af94810037c165fefe8ca05147dea4
Kyohei-us <kyohei1212000@gmail.com>
2019-10-23 15:25:09 -0700

    first commit to heroku

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ bot = Bot(command_prefix='-')
 async def on_ready():
     print("I'm ready.")
     target_channel
-    target_channel = bot.get_channel(635916613320835099)
+    target_channel = bot.get_channel(636689612659490816)
     await target_channel.send("I'm ready.")
 
 bot.run(os.environ.get('BOT_TOKEN'))
